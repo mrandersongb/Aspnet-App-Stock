b77d6306a1b9bf0a04737690276c38c480151e2d
mranderson86 <mr.anderson.brito@gmail.com>
2020-01-30 14:41:02 -0300

    Criado UserCompanies Model

---
+++ Entities/Menu/UserCompanies.cs
@@ -0,0 +1,9 @@
+
+namespace Backend.Entities.Menu {
+    public class UserCompanies {
+
+        public int Id { get; set; }
+        public int Id_Usuario { set; get; }
+        public string Id_Empresa { get;set; }
+    }
+}
